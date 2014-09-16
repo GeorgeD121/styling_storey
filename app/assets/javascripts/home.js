21bed3274d95f36508e2afc3c5b439ff46e5251c
GeorgeD121 <georgedunning@georges-mbp.home>
2014-09-16 12:38:42 +0100

    image changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(document).scrollTop() > 70) {
+        if ($(document).scrollTop() > 50) {
             $("nav").addClass("scrolled");
         } else {
             $("nav").removeClass("scrolled");
